fe67f94ba524eec7bd651d0b0a7699dfc6a4cebd
Miles Bader <milesdbader@gmail.com>
2020-02-12 12:00:41 +0200

    ingredient seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'cleaning Ingredient database'
+
+Ingredient.destroy_all
+
+puts 'ingredient database is clean'
+puts 'generating seed ingredients'
+
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialized = open(url).read
+ingredients = JSON.parse(ingredients_serialized)["drinks"].map { |ingredient| ingredient["strIngredient1"] }
+
+ingredients.each do |ingredient|
+  Ingredient.create(name: ingredient)
+end
+
+puts "ingredients generated; you now have #{Ingredient.count} ingredients"
